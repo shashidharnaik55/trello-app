bc209c3eac045199f25940f32ce661787e2a1b2f
shashidharnaik55 <shashidharnaik55@gmail.com>
2021-04-11 12:15:31 +0530

    Updates

---
+++ precache-manifest.3032c3b5a9dea5f9dc8bed8234a27000.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a75537f7efaf80016c651f060c47390",
+    "url": "/trello-app/index.html"
+  },
+  {
+    "revision": "58fdae1f0469b5bb515b",
+    "url": "/trello-app/static/css/main.81f1aaa3.chunk.css"
+  },
+  {
+    "revision": "5d92d63da03288ce4888",
+    "url": "/trello-app/static/js/2.688b5a34.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trello-app/static/js/2.688b5a34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58fdae1f0469b5bb515b",
+    "url": "/trello-app/static/js/main.5b889ed3.chunk.js"
+  },
+  {
+    "revision": "e40824c85ff22ead0bf0",
+    "url": "/trello-app/static/js/runtime-main.79a25e2c.js"
+  }
+]);
